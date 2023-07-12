15b92e768bcfd47c960ce6e8340190579ea41feb
neyryvargasd <neyry007@gmail.com>
2023-07-12 00:45:24 -0500

    mecanismo Promise js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+const pokedex = document.getElementById('poke-container');
+const promesas =[];
+
 
 for(let i=1; i<=150; i++){
 	const url =`https://pokeapi.co/api/v2/pokemon/${i}`
-	fetch(url)
-	    .then(responsive => responsive.json)
-	    .then(pokemon => {
-			console.log(pokemon)
-		})
+	promesas.push(fetch(url) .then(res => res.json()));
 
 	     
 }
 
+Promise.all(promesas).then(resultados =>{
+	const pokemons = resultados.map((result) =>  ({
+		name :result.name,
+		id : result.id,
+		img : result.sprite. front_defoult,
+		type : result.types.map(type => type.type.name)
+
+	}))
+
+	sPokemons()
+})
+
+const sPokemons = pokemon =>
